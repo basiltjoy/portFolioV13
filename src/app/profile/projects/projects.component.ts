4bea3bb228d9dcf41465ada042706972df071d04
Basil T Joy <basiltjoy96@gmail.com>
2024-05-25 00:10:37 +0530

    Check

--- src/app/profile/projects/projects.component.ts
+++ src/app/profile/projects/projects.component.ts
@@ -12,8 +12,7 @@ export class ProjectsComponent implements OnInit {
   isChecked: boolean = true;
   ToDO: boolean = true;
   SM: boolean = false;
-  Mediclinic: boolean = false;
-  
+ 
   constructor() { }
 
   ngOnInit(): void {
@@ -21,21 +20,15 @@ export class ProjectsComponent implements OnInit {
     this.ToDO = true;
   }
 
-  ProjectList(event: any) { 
-    if (event.target['name'] == 'ToDo') { 
-      this.ToDO = true; 
-      this.SM = false; 
-      this.Mediclinic = false; 
-    } 
-    else if (event.target['name'] == 'SM') {
-      this.SM = true; 
-      this.ToDO = false; 
-      this.Mediclinic = false; 
-    } else { 
-      this.Mediclinic = true; 
-      this.SM = false; 
-      this.ToDO = false; 
-    } 
+  ProjectList(event: any) {
+    if (event.target['name'] == 'ToDo') {
+      this.ToDO = true;
+      this.SM = false
+    }
+    else {
+      this.SM = true
+      this.ToDO = false;
+    }
   }
 
 
